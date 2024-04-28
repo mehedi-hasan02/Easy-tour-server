09cabfd27f02d48d5973bb4c9f91fe2a04e6af40
mehedi-hasan02 <mehedisani02@gmail.com>
2024-04-28 19:48:50 +0600

    make some change country api

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fvwg0tw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -24,23 +23,15 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
 
     const database = client.db("touristsDB");
     const tourists = database.collection("tourists");
-    // const countryDB = database.collection("countryDB");
+    
 
     app.post('/tourist', async (req, res) => {
       const newSpot = req.body;
       const result = await tourists.insertOne(newSpot);
-      // const countryData = {
-      //   cImage : newSpot.image,
-      //   cName : newSpot.country,
-      //   cDescription : newSpot.shortDescription,
-
-      // };
-      // const cDbCollection = await countryDB.insertOne(countryData);
+      
       res.send(result);
     })
 
@@ -104,13 +95,9 @@ async function run() {
     const result = await tourists.find({ userEmail: email }).toArray();
     res.send(result);
     })
-
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
